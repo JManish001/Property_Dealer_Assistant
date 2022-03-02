9c27856aa5834d53b8be807faa890879a5685958
Manish Jindal <99240049+JManish001@users.noreply.github.com>
2022-03-02 13:25:22 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,32 @@
-package application;
-	
+package customer;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-
-public class Main extends Application {
+public class Main extends Application 
+{
 	@Override
-	public void start(Stage primaryStage) {
+ public void start(Stage primaryStage) 
+   {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+				Parent root=FXMLLoader.load(getClass().getResource("CustomerRegisterView.fxml")); 
+				Scene scene = new Scene(root,700,600);
+				//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				primaryStage.setScene(scene);
+				primaryStage.show();
+		    } 
+		catch(Exception e)
+			{
+				e.printStackTrace();
+			}
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		launch(args);
 	}
 }
+

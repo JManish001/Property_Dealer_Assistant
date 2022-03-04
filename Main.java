1c0c15ce6678587a694e73d49be5d645bb2aed67
Manish Jindal <99240049+JManish001@users.noreply.github.com>
2022-03-04 12:26:09 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package dealsMatured;
+package galleryPic;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,8 +12,8 @@ public class Main extends Application
  public void start(Stage primaryStage) 
    {
 		try {
-				Parent root=FXMLLoader.load(getClass().getResource("dealsMaturedView.fxml")); 
-				Scene scene = new Scene(root,500,550);
+				Parent root=FXMLLoader.load(getClass().getResource("galleryPic.fxml")); 
+				Scene scene = new Scene(root,550,700);
 				//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 				primaryStage.setScene(scene);
 				primaryStage.show();

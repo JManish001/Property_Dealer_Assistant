1c666cc9ca86f405cb61e72e34fe40a7f514d0f6
Manish Jindal <99240049+JManish001@users.noreply.github.com>
2022-03-07 10:38:55 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package searchProperty;
+package signup;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,8 +12,8 @@ public class Main extends Application
  public void start(Stage primaryStage) 
    {
 		try {
-				Parent root=FXMLLoader.load(getClass().getResource("searchPropertyView.fxml")); 
-				Scene scene = new Scene(root,600,700);
+				Parent root=FXMLLoader.load(getClass().getResource("signupForm.fxml")); 
+				Scene scene = new Scene(root,550,500);
 				//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 				primaryStage.setScene(scene);
 				primaryStage.show();
